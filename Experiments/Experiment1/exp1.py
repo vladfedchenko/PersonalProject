1c9c8f812a3d03cbccff668d32f8c7cd78ff7648
vladyslav fedchenko <vfedchen@ghibli.inria.fr>
2017-11-22 12:03:57 +0100

    Experiment 1 improved

--- Experiments/Experiment1/exp1.py
+++ Experiments/Experiment1/exp1.py
@@ -1,16 +1,22 @@
-import find_dependency as fd
-import statistics_utils as su
+import os
 import numpy as np
 import sys
 import random
 import matplotlib.pyplot as plt
+import datetime
 
-experiment_number = 10**4
+sys.path.append(os.getcwd())
+
+import find_dependency as fd
+import statistics_utils as su
+
+experiment_number = 10**2
 bins_num = 50
 
-exp_start = 60
+exp_start = 50
 exp_end = 61
 exp_step = 10
+
 
 def select_random_spaced_cols(vec_size, line_len):
     population = range(line_len)
@@ -28,19 +34,38 @@ def select_random_spaced_cols(vec_size, line_len):
 
     return to_ret
 
+
+def run_experiments(vec_size, experiment_num, line_length, lines, answers):
+    print "Experiment K={0} started: {1}".format(vec_size, datetime.datetime.now())
+    accuracy_list = []
+    for cur_exp in xrange(experiment_num):
+        if cur_exp != 0 and cur_exp % 1000 == 0:
+            print "Completed: ({0}, {1})".format(vec_size, cur_exp)
+        cols = select_random_spaced_cols(vec_size, line_length)
+        mapping = fd.generate_mapping(lines, answers, cols)
+        accuracy = fd.evaluate_mapping_accuracy(lines, answers, cols, mapping)
+        accuracy_list.append(accuracy)
+
+    print "Experiment K={0} finished: {1}".format(vec_size, datetime.datetime.now())
+    return accuracy_list
+
+
+def find_percentile(values, test_value):
+    values = np.array(values)
+    less_or_equal = values[values <= test_value]
+    return float(len(less_or_equal)) * 100.0 / len(values)
+
+
 def main():
     lines_rectangle, correct_responces, lengths_list = fd.prepare_rectangle_data(sys.argv)
     if not (lines_rectangle is None):
         cur_figure = 1
         for vec_size in range(exp_start, exp_end, exp_step):
-            accuracy_list = []
-            for cur_exp in xrange(experiment_number):
-                if cur_exp != 0 and cur_exp % 1000 == 0:
-                    print "Completed: ({0}, {1})".format(vec_size, cur_exp)
-                cols = select_random_spaced_cols(vec_size, lengths_list[2])
-                mapping = fd.generate_mapping(lines_rectangle, correct_responces, cols)
-                accuracy = fd.evaluate_mapping_accuracy(lines_rectangle, correct_responces, cols, mapping)
-                accuracy_list.append(accuracy)
+            accuracy_list = run_experiments(vec_size
+                                            , experiment_number
+                                            , lengths_list[2]
+                                            , lines_rectangle
+                                            , correct_responces)
 
             #minimal distance approach
             totat_variation_dist = su.calculate_total_var_dist(lines_rectangle, lengths_list[2])
@@ -50,16 +75,19 @@ def main():
             test_mapping = fd.generate_mapping(lines_rectangle, correct_responces, test_cols)
             test_accuracy = fd.evaluate_mapping_accuracy(lines_rectangle, correct_responces, test_cols, test_mapping)
 
+            percentile = int(find_percentile(accuracy_list, test_accuracy))
+
+            #plotting the histogram
             fig = plt.figure(cur_figure, figsize=(6, 6))
             cur_figure += 1
-            fig.suptitle('K = {0}'.format(vec_size))
+            fig.suptitle('K = {0}\nTest accuracy: {1}, Percentile: {2}'.format(vec_size, test_accuracy, percentile))
             plt.hist(accuracy_list, bins=bins_num)
             plt.axvline(test_accuracy, c='r', label='Test accuracy')
             plt.xlabel('Accuracy')
             plt.ylabel('Appearances')
-            fig.show()
+            plt.ioff()
+            plt.savefig("Experiments/Experiment1/k{0}.png".format(vec_size))
 
-    raw_input()
 
 if __name__ == "__main__":
     main()
