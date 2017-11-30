b7b4a30c23808ccc5b941d65a3a3d9a2229f8e5c
vladfedchenko <vladfedchenko@gmail.com>
2017-11-30 11:46:41 +0100

    Experiment 4 script finished

--- Experiments/Experiment4/exp4.py
+++ Experiments/Experiment4/exp4.py
@@ -15,6 +15,9 @@ exp_step = 5
 max_improvement_steps = 10 ** 3
 
 max_mapping_distance = 3
+max_col_distance = 3
+
+improvement_print_freq = 1
 
 
 def test_comb_improve_mapping(lines, answers, working_cols, mapping, accuracy, letters_to_change):
@@ -35,17 +38,60 @@ def try_improve_mapping(lines, answers, working_cols, mapping, accuracy):
     alpha_len = len(working_alphabet)
     for distance in range(1, max_mapping_distance + 1):
         change_comb = range(distance)
-        while not(change_comb is None):
+        while not (change_comb is None):
             letters_to_change = [working_alphabet[i] for i in change_comb]
-            new_acc, new_mapping = test_comb_improve_mapping(lines, answers, working_cols, mapping, accuracy, letters_to_change)
-            if not(new_acc is None):
+            new_acc, new_mapping = test_comb_improve_mapping(lines
+                                                             , answers
+                                                             , working_cols
+                                                             , mapping
+                                                             , accuracy
+                                                             , letters_to_change)
+            if not (new_acc is None):
                 return True, new_acc, new_mapping
             change_comb = su.get_next_combination(change_comb, distance, alpha_len)
     return False, None, None
 
+
+def test_comb_improve_cols(lines, answers, working_cols, mapping, accuracy, change_comb, unused_cols):
+    unused_len = len(unused_cols)
+    change_len = len(change_comb)
+    unused_comb = range(change_len)
+    while not (unused_comb is None):
+        working_cols_copy = working_cols[:]
+        for i in range(change_len):
+            working_cols_copy[change_comb[i]] = unused_cols[unused_comb[i]]
+        new_acc = fd.evaluate_mapping_accuracy(lines, answers, working_cols_copy, mapping)
+        if new_acc > accuracy:
+            return new_acc, working_cols_copy
+        unused_comb = su.get_next_combination(unused_comb, change_len, unused_len)
+    return None, None
+
+
+def try_improve_columns(lines, answers, working_cols, mapping, line_len, accuracy):
+    unused_cols = [i for i in range(line_len) if i not in working_cols]
+    cols_len = len(working_cols)
+    for distance in range(1, max_col_distance + 1):
+        if len(unused_cols) >= distance:
+            change_comb = range(distance)
+            while not (change_comb is None):
+                new_acc, new_cols = test_comb_improve_cols(lines
+                                                           , answers
+                                                           , working_cols
+                                                           , mapping
+                                                           , accuracy
+                                                           , change_comb
+                                                           , unused_cols)
+                if not (new_acc is None):
+                    return True, new_acc, new_cols
+                change_comb = su.get_next_combination(change_comb, distance, len(working_cols))
+        else:
+            break
+    return False, None, None
+
+
 def improve_solution(lines, answers, working_cols, mapping, line_len):
     accuracy_list = []
-    not_improved_counter = 0;
+    not_improved_counter = 0
     accuracy = fd.evaluate_mapping_accuracy(lines, answers, working_cols, mapping)
     accuracy_list.append(accuracy)
     step_counter = 0
@@ -57,23 +103,21 @@ def improve_solution(lines, answers, working_cols, mapping, line_len):
             mapping = new_mapping
             accuracy_list.append(accuracy)
             step_counter += 1
-            if step_counter % 10 == 0:
-                print 'Times improved: {0}/{1}'.format(step_counter, max_improvement_steps)
+            if step_counter % improvement_print_freq == 0:
+                print 'Times improved: {0}/{1}, {2}'.format(step_counter, max_improvement_steps, datetime.datetime.now())
         else:
             not_improved_counter += 1
-        # improved, new_acc, new_cols = try_improve_columns(lines, answers, working_cols, mapping, line_len, accuracy)
-        # if improved:
-        #     not_improved_counter = 0
-        #     accuracy = new_acc
-        #     working_cols = new_cols
-        #     accuracy_list.append(accuracy)
-        #     step_counter += 1
-        #     if step_counter % 10 == 0:
-        #         print 'Times improved: {0}/{1}'.format(step_counter, max_improvement_steps)
-        # else:
-        #     not_improved_counter += 1
-
-
+        improved, new_acc, new_cols = try_improve_columns(lines, answers, working_cols, mapping, line_len, accuracy)
+        if improved:
+            not_improved_counter = 0
+            accuracy = new_acc
+            working_cols = new_cols
+            accuracy_list.append(accuracy)
+            step_counter += 1
+            if step_counter % improvement_print_freq == 0:
+                print 'Times improved: {0}/{1}, {2}'.format(step_counter, max_improvement_steps, datetime.datetime.now())
+        else:
+            not_improved_counter += 1
 
     return accuracy_list
 
@@ -92,7 +136,7 @@ def main():
             mapping = fd.generate_mapping(lines_rectangle, correct_responces, working_cols)
             accuracy_list = improve_solution(lines_rectangle, correct_responces, working_cols, mapping, line_len)
 
-            #plotting
+            # plotting
             fig = plt.figure(cur_figure, figsize=(10, 6))
             fig.suptitle('C = {1}, K = {0}'.format(vec_size, line_len))
             cur_figure += 1
@@ -100,7 +144,6 @@ def main():
             plt.savefig("Experiments/Experiment4/k{0}.png".format(vec_size))
 
             print "Experiment K={0} finished: {1}".format(vec_size, datetime.datetime.now())
-
 
 
 if __name__ == "__main__":
